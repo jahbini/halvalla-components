59003b8cdf1c320a4926c3d70b9baa3dd4370e79
Anthony Davie <adavie1@users.noreply.github.com>
2017-02-26 11:10:32 +0800

    3.6.0
    Next release will include mapping features.
    Usage: https://riotgear2.github.io/components/address/

--- dist/rg-address/rg-address.js
+++ dist/rg-address/rg-address.js
@@ -1,1 +1,12 @@
+    riot.tag("rg-address",
+        '<address class="c-address">'+
+        '  <span class="c-address__heading">{opts.address.recipient}</span>'+
+        '  <div each="{addr in opts.address.lines}">'+
+        '     <span class="{\'c-address--road\':addr.road} {\'c-address--district\':addr.district} {\'c-address--town\':addr.town} {\'c-address--code\':addr.code} {\'c-address--state\':addr.state} {\'c-address--country\':addr.country}">{addr.line}<br></span>'+
+        '  </div>'+
+        '</address>',
+        '',
+        function() {
+          var _this = this ;
 
+        });
