37cadc63757138c587790a9fb80b5908a5a870a6
Anthony Davie <adavie1@users.noreply.github.com>
2017-03-03 15:24:54 +0800

    Updated to 3.6.1

--- dist/rg-utils.js
+++ dist/rg-utils.js
@@ -1,25 +1,91 @@
-// START: --- RG_UTILS code ------
-var rg_date_cdn_momentjs = "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.min.js";
+var RG2_VERSION = "3.6.1" ;
+var RG2_INIT = false ;
+var RG2_BASE = Date.now() ;
 
-//var rg_chart_cdn_chartjs = "https://cdn.jsdelivr.net/chart.js/1.0.2/Chart.min.js" ;
-var rg_chart_cdn_chartjs = "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js";
-var rg_credit_card_payment_fonts = "https://cdnjs.cloudflare.com/ajax/libs/paymentfont/1.1.2/css/paymentfont.min.css";
-var rg_markdown_cdn_markdown = "https://cdnjs.cloudflare.com/ajax/libs/markdown-it/8.3.0/markdown-it.min.js";
+var RG2_MKDN_RENDER ;
 
-function loadJS(file, callback) {
-    // DOM: Create the script element
-    var script = document.createElement("script");
-    // set the type attribute
-    script.type = "application/javascript";
-    // make the script element load file
-    script.src = file;
-    script.onload = callback;
-    script.onreadystatechange = callback;
-    // finally insert the element to the body element in order to load the script
-    document.head.appendChild(script);
+var RG_DATE_CDN_MOMENTJS = "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.min.js";
+
+//var RG_CHART_CDN_CHARTJS = "https://cdn.jsdelivr.net/chart.js/1.0.2/Chart.min.js" ;
+var RG_CHART_CDN_CHARTJS = "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js";
+var RG_CREDIT_CARD_PAYMENT_FONTS = "https://cdnjs.cloudflare.com/ajax/libs/paymentfont/1.1.2/css/paymentfont.min.css";
+
+  var RG_MARKDOWN_CDN_MARKDOWN = "https://cdnjs.cloudflare.com/ajax/libs/markdown-it/8.3.0/markdown-it.min.js";
+
+
+  // ----------------------------------------
+
+function RGVersion() {
+  return RG2_VERSION ;
 }
 
+  // ----------------------------------------
+function loadCSS(file, callback, error) {
+    var _file = file ;
+    var loaded = document.querySelector('link[href="'+file+'"]') ;
 
+    if (loaded) {
+      loaded.onload = callback ;
+      loaded.onreadystatechange = callback;
+      return
+    }
+
+    var css = document.createElement("script");
+    css.type = "text/css";
+    css.rel = "stylesheet";
+    css.href = file;
+    script.onload = callback ;
+    loaded.onreadystatechange = callback;
+
+    if (error) {
+       css.onerror = error ;
+       }
+    else {
+       css.onerror = function(e) {
+              console.error("CSS File '" + _file + "' not found :-(");
+              };
+       }
+
+    document.head.appendChild(css);
+}   //eof: loadCSS
+
+
+// ----------------------------------------
+function loadJS(file, callback, error, type) {
+    var _file = file ;
+    var loaded = document.querySelector('script[src="'+file+'"]') ;
+
+    if (loaded) {
+      loaded.onload = callback ;
+      loaded.onreadystatechange = callback;
+      return
+    }
+
+    var script = document.createElement("script");
+
+    script.type = (typeof type ==="string" ? type : "application/javascript") ;
+
+    script.src = file;
+    script.async = false ;
+    script.defer = false ;
+    script.onload = callback ;
+
+    if (error) {
+       script.onerror = error ;
+       }
+    else {
+       script.onerror = function(e) {
+              console.error("Script File '" + _file + "' not found :-(");
+              };
+       }
+
+    script.onreadystatechange = callback;
+
+    document.body.appendChild(script);
+}   // eof: loadJS
+
+
+// ----------------------------------------
 function toBoolean(bool) {
     if (bool) {
         if (typeof bool === "string") {
@@ -33,5 +99,76 @@ function toBoolean(bool) {
     } else
         return undefined;
 
+}   // eof: toBoolean
+
+// ----------------------------------------
+function stripParas (text) {
+  if (text) {
+     text = text.replace(/<\/?p[^>]*>/g, '') ;
+   }
+return text ;
 }
-// END: --- RG_UTILS code ------
+
+
+// ----------------------------------------
+function toMarkdown (content, renderCallback) {
+  var markdownOK = !(typeof markdownit === "undefined");
+  RG2_MKDN_RENDER = (markdownOK ? markdownit({html: true, linkify: true, typographer: true}) : undefined ) ;
+  var _content = content ;
+
+
+  var callbackMkdn = function callbackMkdn() {
+      markdownOK = !(typeof markdownit === "undefined");
+
+      if (!RG2_MKDN_RENDER && markdownOK && _content) {
+          RG2_MKDN_RENDER = markdownit({
+              html: true,
+              linkify: true,
+              typographer: true
+          });
+
+          if (markdownOK) {
+             rendered = RG2_MKDN_RENDER.render(_content) ;
+
+             rendered = stripParas(rendered) ;
+
+             if (renderCallback)
+                renderCallback(rendered) ;
+
+             return rendered ;
+             }
+
+      }
+  }   // eof: toMarkdown::callbackMkdn
+
+  if ( ! markdownOK)
+     loadJS(RG_MARKDOWN_CDN_MARKDOWN, callbackMkdn);
+  else {
+     rendered = RG2_MKDN_RENDER.render(_content) ;
+     console.log("markdown output:" + rendered) ;
+
+     if (renderCallback)
+        renderCallback(rendered) ;
+
+     return stripParas(rendered) ;
+     }
+}    // eof: toMarkdown
+
+// ----------------------------------------
+function RiotGearInit() {
+    if (RG2_INIT === true)
+       return ;
+
+    var localCB = function (text) {
+    console.log(text) ;
+    RG2_INIT = true ;
+    }
+
+ toMarkdown("**RG2** [" + RG2_VERSION + "] started", localCB) ;
+}   // eof: RiotGearInit
+
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  RiotGearInit() ;
+});
